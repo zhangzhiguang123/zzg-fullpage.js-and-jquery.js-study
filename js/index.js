89242e0b4c988fcdd1623572ff2275bf0696224e
zhangzhiguang123 <1339435196@qq.com>
2019-09-18 21:49:42 +0800

    fullpage插件项目

---
+++ js/index.js
@@ -0,0 +1,99 @@
+$(function () {
+    $("#fullpage").fullpage({
+        navigation: true,
+        onLeave: function(index, nextIndex, direction){
+             if(index){
+                 $("aside>img").animate({
+                    opacity:"0",
+                 },300);
+             }
+        },
+        afterLoad:function(anchorLink, index){
+            if(index){
+                $("aside>img").delay("3000").animate({
+                   opacity:"1",
+                },300);
+            }
+            if(index==2){
+                $("#two .searchimg img:first-child")
+                .css("display","block")
+                .animate({
+                        left: "41%",
+                },600)
+                .animate(
+                    {
+                        left:"43%",                 
+                    },400
+                )
+                .animate(
+                    {
+                        opacity:"0",
+                    },400
+                );
+                $("#two .searchimg img:last-child").delay(1000)
+                .animate({
+                    opacity:"1",
+                },400)
+                .animate(
+                    {
+                        width:"150px",
+                        height:"30px",
+                        left:"57%",
+                        top:"238px",
+                    },1600
+                )
+                $("#two .tablebox img:last-child").delay(1400)
+                .animate({
+                       width:"410px",
+                       height:"220px",
+                },1600);
+                $("#two .fontimg img:last-child").delay(1400)
+                .animate({
+                    opacity:"1",
+                }
+                ,1100)
+            }
+            
+        }
+        })
+});
+// 第一页效果
+$(function(){
+    towardDown();
+    towardDown1();
+    function towardDown() {
+        $("#one img:nth-child(1)").animate({
+            top: "150px",
+        }, 1500, "linear", towardUp);
+    }
+    function towardUp() {
+        $("#one img:nth-child(1)").animate({
+            top: "80px",
+        }, 1500, "linear", towardDown);
+    }
+    function towardDown1() {
+        $("#one img:nth-child(2)").animate({
+            bottom: "350px",
+        }, 1100, "linear", towardUp1);
+    }
+    function towardUp1() {
+        $("#one img:nth-child(2)").animate({
+            bottom: "400px",
+        }, 1100, "linear", towardDown1);
+    }
+});
+
+//侧边栏跳动
+$(function(){
+    towardDown2();
+    function towardDown2() {
+        $("aside>img").animate({
+            bottom: "70px",
+        }, 400, "linear", towardUp2);
+    }
+    function towardUp2() {
+        $("aside>img").animate({
+            bottom: "100px",
+        }, 400, "linear", towardDown2);
+    } 
+})
